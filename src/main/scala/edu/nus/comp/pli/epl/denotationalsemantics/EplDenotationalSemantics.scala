6c0c247a33e08d4851887e223989d7a5e7017afc
jburkeedu <1Hillheaddrive>
2019-03-01 15:00:36 +0800

    Initial commit

---
+++ src/main/scala/edu/nus/comp/pli/epl/denotationalsemantics/EplDenotationalSemantics.scala
@@ -0,0 +1,39 @@
+package edu.nus.comp.pli.epl.denotationalsemantics
+
+import edu.nus.comp.pli.epl.parser.EplAST._
+import edu.nus.comp.pli.epl.evm.EvmInstruction._
+
+object EplDenotationalSemantics {
+  private def compileHelper(expr: Expression): Seq[Instruction] = expr match {
+    case Bin(Add, left, right) => compileHelper(left) ++ compileHelper(right) :+ PLUS
+    /*add your code for sub,mul,div and pow*/
+    case Bin(Sub, left, right) => compileHelper(left) ++ compileHelper(right) :+ MINUS
+    case Bin(Mul, left, right) => compileHelper(left) ++ compileHelper(right) :+ TIMES
+    case Bin(Div, left, right) => compileHelper(left) ++ compileHelper(right) :+ DIV
+    case Bin(Pow, left, right) => compileHelper(left) ++ compileHelper(right) :+ POW
+    case Bin(And, left, right) =>  compileHelper(left) ++ compileHelper(right) :+ AND
+    /*add your code for or*/
+    case Bin(Or, left, right) => compileHelper(left) ++ compileHelper(right) :+ OR
+    case Bin(Lt, left, right) => compileHelper(left) ++ compileHelper(right) :+ LT
+    /*add your code for lte*/
+    case Bin(Lte, left, right) => compileHelper(left) ++ compileHelper(right) :+ LE
+    case Bin(Gt, left, right) =>  compileHelper(left) ++ compileHelper(right) :+ GT
+    /*add your code for gte*/
+    case Bin(Gte, left, right) => compileHelper(left) ++ compileHelper(right) :+ GE
+    case Bin(Eq, left, right) =>  compileHelper(left) ++ compileHelper(right) :+ EQ
+    /*add your code for NEq*/
+    case Bin(NEq, left, right) => compileHelper(left) ++ compileHelper(right) :+ NEQ
+    case Ury(Not, inner) => compileHelper(inner) :+ NOT
+    /*add your code for neg*/
+    case Ury(Minus, inner) => compileHelper(inner) :+ NEG
+    case Num(value) => Seq(LDCI(value))
+    /*add your code for bool value*/
+    case Bool(value) => Seq(LDCB(value))
+
+    /*add your code for if else statement*/
+    case IfElse (cond, ifb, elseb) => compileHelper(cond) ++ compileHelper(ifb) ++ compileHelper(elseb) :+ IFELSE
+  }
+
+
+  def compile(expr: Expression) = compileHelper(expr) :+ DONE
+}
---
+++ src/main/scala/edu/nus/comp/pli/epl/evm/EvmExec.scala
@@ -0,0 +1,96 @@
+package edu.nus.comp.pli.epl.evm
+
+import edu.nus.comp.pli.epl.bigstepsemantics.EplBigStepSemantics.{Result, ResultBool, ResultInt}
+import edu.nus.comp.pli.epl.evm.EvmInstruction._
+import scala.math.pow
+
+case class EvmState(program: Seq[Instruction], opdStack: List[Result]) {
+  def isDone: Boolean = program.headOption == Option(DONE)
+
+  def result: Result = {
+    assert(isDone)
+    opdStack.head
+  }
+
+  def transition: EvmState = (program, opdStack) match {
+    case (PLUS :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultInt(n1 + n2) :: restOpds)
+
+    case (MINUS :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultInt(n1 - n2) :: restOpds)
+
+    case (TIMES :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultInt(n1 * n2) :: restOpds)
+
+    /*add your code for div and pow*/
+    case (DIV :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultInt(n1 / n2) :: restOpds)
+    case (POW :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultInt(pow(n1,n2).toInt) :: restOpds)
+    case (AND :: restProgram, ResultBool(b2) :: ResultBool(b1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(b1 && b2) :: restOpds)
+
+    case (OR :: restProgram, ResultBool(b2) :: ResultBool(b1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(b1 || b2) :: restOpds)
+
+    case (LT :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 < n2) :: restOpds)
+
+    case (LE :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 <= n2) :: restOpds)
+
+    case (GT :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 > n2) :: restOpds)
+
+    case (GE :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 >= n2) :: restOpds)
+
+    /*add your code for EQ*/
+    case (EQ :: restProgram, ResultBool(n2) :: ResultBool(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 == n2) :: restOpds)
+
+    case (EQ :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 == n2) :: restOpds)
+
+    case (NEQ :: restProgram, ResultInt(n2) :: ResultInt(n1) :: restOpds) =>
+      EvmState(restProgram, ResultBool(n1 != n2) :: restOpds)
+
+    case (NOT :: restProgram, ResultBool(b) :: restOpds) =>
+      EvmState(restProgram, ResultBool(!b) :: restOpds)
+
+    case (NEG :: restProgram, ResultInt(n) :: restOpds) =>
+      EvmState(restProgram, ResultInt(- n) :: restOpds)
+
+    case (LDCI(n) :: restProgram, opds) =>
+      EvmState(restProgram, ResultInt(n) :: opds)
+
+    /*add your code for bool*/
+    case (LDCB(b) :: restProgram, opds) =>
+      EvmState(restProgram, ResultBool(b) :: opds)
+    /*add your code for if else statement
+    * hint:
+    * 1) be careful with the order of
+    * 2) the type of if-branch and else-branch should be the same
+    * */
+
+    case (IFELSE ::restProgram,  (ResultInt(n2)) ::(ResultInt(n1)) :: (ResultBool(b3)) ::restOpds) => {
+      if(ResultBool(b3) == ResultBool(true)) {
+        EvmState(restProgram, (ResultInt(n1)):: restOpds)
+      } else {
+        EvmState(restProgram, (ResultInt(n2)):: restOpds)
+      }
+    }
+
+    case (IFELSE ::restProgram,  (ResultBool(elseb)) ::(ResultBool(ifb)) :: (ResultBool(cond)) ::restOpds) => {
+      if(ResultBool(cond) == ResultBool(true)) {
+        EvmState(restProgram, (ResultBool(ifb)):: restOpds)
+      } else {
+        EvmState(restProgram, (ResultBool(elseb)):: restOpds)
+      }
+    }
+
+  }
+
+  def execute: Result =
+    Iterator.iterate(this)(_.transition).dropWhile(! _.isDone).map(_.result).next
+}
